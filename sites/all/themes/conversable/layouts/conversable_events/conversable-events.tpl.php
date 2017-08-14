b8f268f304f6e47884a8247ce1e367187c9f4e37
iirenesg <irenepsg10@gmail.com>
2017-08-14 16:46:57 -0600

    Agrego estilos de sección Next Events

--- sites/all/themes/conversable/layouts/conversable_events/conversable-events.tpl.php
+++ sites/all/themes/conversable/layouts/conversable_events/conversable-events.tpl.php
@@ -15,7 +15,7 @@
 ?>
 <div class="" <?php if (!empty($css_id)) {print "id=\"$css_id\"";} ?>>
   <div class="">
-    <div><?php print $content['top']; ?></div>
-    <div><?php print $content['single_wide']; ?></div>
+    <div class="l-group section-wrap bg-pattern-turquoise"><?php print $content['top']; ?></div>
+    <div class="l-group section-wrap group"><?php print $content['single_wide']; ?></div>
   </div>
 </div>

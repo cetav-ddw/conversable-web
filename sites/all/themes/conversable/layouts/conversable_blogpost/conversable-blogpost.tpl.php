db8232f7c518d4e2461d8d219b7b29022acb1ac9
Maria Jose Solis <majosm96@gmail.com>
2017-08-22 11:49:12 -0600

    Nuevos layouts blosgPost/Event

---
+++ sites/all/themes/conversable/layouts/conversable_blogpost/conversable-blogpost.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Template for One page panel layout.
+ *
+ * This template provides a homepage for CONVERSABLE panel display layout.
+ *
+ * Variables:
+ * - $id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout. This layout supports the following sections:
+ *   $content['left_above']: Content in the left above section.
+ *   $content['right_above']: Content in the right above section.
+ *   $content['bottom']: Content in the bottom.
+ */
+?>
+<div class="" <?php if (!empty($css_id)) {print "id=\"$css_id\"";} ?>>
+  <div class="">
+    <div class="l-group section-wrap"><?php print $content['main']; ?></div>
+    <div class="l-group section-wrap"><?php print $content['bottom']; ?></div>
+  </div>
+</div>

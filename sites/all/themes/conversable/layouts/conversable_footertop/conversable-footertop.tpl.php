c2f884b64336cc758b3495d8acaff142a78f1101
Maria Jose Solis <majosm96@gmail.com>
2017-08-24 13:10:41 -0600

    Nuevo layout footer top

---
+++ sites/all/themes/conversable/layouts/conversable_footertop/conversable-footertop.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Template for One page panel layout.
+ *
+ * This template provides a homepage for CONVERSABLE panel display layout.
+ *
+ * Variables:
+ * - $id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout. This layout supports the following sections:
+ *   $content['main']: Content in the left above section.
+ */
+?>
+<div class="" <?php if (!empty($css_id)) {print "id=\"$css_id\"";} ?>>
+  <div class="">
+    <div class="l-group section-wrap"><?php print $content['main']; ?></div>
+  </div>
+</div>

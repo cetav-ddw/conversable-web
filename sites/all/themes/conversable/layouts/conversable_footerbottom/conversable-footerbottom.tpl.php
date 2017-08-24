d7d84d6ba29d5f40f4ac1fb29dec540f2043962b
Maria Jose Solis <majosm96@gmail.com>
2017-08-24 13:05:55 -0600

    Nuevo layout footer bottom

--- sites/all/themes/conversable/layouts/conversable_footerbottom/conversable-footerbottom.tpl.php
+++ sites/all/themes/conversable/layouts/conversable_footerbottom/conversable-footerbottom.tpl.php
@@ -9,15 +9,13 @@
  * - $id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
  *   panel of the layout. This layout supports the following sections:
-*   $content['top']: Content in the top.
- *   $content['left_below']: Content in the left above section.
- *   $content['right_below']: Content in the right above section.
+ *   $content['left']: Content in the left above section.
+ *   $content['right']: Content in the right above section.
  */
 ?>
 <div class="" <?php if (!empty($css_id)) {print "id=\"$css_id\"";} ?>>
   <div class="">
-    <div class="l-group section-wrap"><?php print $content['top']; ?></div>
-    <div class="l-group section-wrap"><?php print $content['left_below']; ?></div>
-    <div class="l-group section-wrap"><?php print $content['right_below']; ?></div>
+    <div class="l-group section-wrap"><?php print $content['left']; ?></div>
+    <div class="l-group section-wrap"><?php print $content['right']; ?></div>
   </div>
 </div>

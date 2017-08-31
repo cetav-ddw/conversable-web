45d2694ccdc811492550430a27fe294a61a5ec48
Jose Leiva <jdleiva@backcountry.com>
2017-08-30 20:39:30 -0600

    Actualizo posicion de sticky

--- sites/all/themes/conversable/js/main.js
+++ sites/all/themes/conversable/js/main.js
@@ -4,31 +4,47 @@ jQuery(document).ready(function ($) {
     //Variables
     var menu = $('#stickyMenu');
     var mScroll = '';
-    
+
     var startScroll = function() {
       $(window).scroll(function () {
         var wScroll = $(this).scrollTop();
-        if(wScroll >= mScroll) {
+        
+        if (wScroll >= mScroll) {
           menu.addClass('sticky-menu-fixed');
         } else {    
           menu.removeClass('sticky-menu-fixed');
         }
-      } );
+      });
+
+      $('.sticky-icon').click(function(event) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+          if (target.length) {
+            event.preventDefault();
+            
+            $('html, body').animate({
+              scrollTop: target.offset().top - 130
+            }, 1000);
+          }
+        }
+      });
     }
     
     //Functions
     var enquireModule = function () {
       // Register when match a viewport of 650px
-      enquire.register('screen and (min-width:650px)', {
-        match: function () {
-          if(menu.length){
+      enquire.register('screen and (min-width: 650px)', {
+        match: function() {
+          if(menu.length) {
             mScroll =  menu.offset().top;
             menu.removeClass('hidden');
             startScroll();
           }
         },
-        unmatch: function () {
-          if(menu.length){
+        unmatch: function() {
+          if(menu.length) {
             menu.addClass('hidden');
             menu.unbind('scroll');
           }
